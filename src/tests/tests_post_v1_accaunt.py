6bcf40a0701f279b735e1c78ea76f5bf5a2be8af
Maksim Ivanov <mivanov@webmonitorx.ru>
2025-08-08 03:23:51 +1000

    Создание класс клиентов

--- src/tests/tests_post_v1_accaunt.py
+++ src/tests/tests_post_v1_accaunt.py
@@ -1,12 +1,18 @@
 import json
 
-import requests
+from apps.dm_api_account.api.account_api import AccountApi
+from apps.dm_api_account.api.login_api import LoginApi
+from apps.api_mailhog.api.mailhog_api import MailhogApi
+
 
 def test_post_v1_account(base_api_url, mail_url):
-    login = 'miv_test4'
+    login = 'miv_test6'
     password = '12345678'
     email = f'{login}@mail.ru'
-    token = None
+
+    account_api = AccountApi(host=base_api_url)
+    login_api = LoginApi(host=base_api_url)
+    mailhog_api = MailhogApi(host=mail_url)
 
     # Регистрация пользователя
     json_data = {
@@ -14,42 +20,41 @@ def test_post_v1_account(base_api_url, mail_url):
         'email': email,
         'password': password,
     }
-
-    response = requests.post(f'{base_api_url}/v1/account', json=json_data)
+    response = account_api.post_v1_account(json_data=json_data)
     assert response.status_code == 201, f'Пользователь не был создан {response.json()}'
 
     # Получить письмо
-    params = {
-        'limit': '10',
-    }
-
-    response = requests.get(f'{mail_url}/api/v2/messages', params=params, verify=False)
+    response = mailhog_api.get_api_v2_messages()
     assert response.status_code == 200, 'Письма не были получены'
 
     # Получить активационный токон
-    for item in response.json()['items']:
-        user_data = json.loads(item['Content']['Body'])
-        user_login = user_data['Login']
-        if user_login == login:
-            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
-            print(f'{token=}')
-            break
+    token = get_activation_token_by_login(login, response)
     assert token is not None, f'Токен для пользователя {login} не был получен'
-    # Активация пользователя
-    headers = {
-        'accept': 'text/plain',
-    }
 
-    response = requests.put(f'{base_api_url}/v1/account/{token}', headers=headers)
+    # Активация пользователя
+    headers, response = account_api.put_v1_account_token(token=token)
     assert response.status_code == 200, f'Пользователь {login} не был активирован'
 
     # Авторизация
-
     json_data = {
         'login': login,
         'password': password,
         'rememberMe': True,
     }
 
-    response = requests.post(f'{base_api_url}/v1/account/login', headers=headers, json=json_data)
+    response = login_api.post_v1_account_login(json_data=json_data)
     assert response.status_code == 200, f'Пользователь {login} не смог авторизоваться'
+
+
+def get_activation_token_by_login(login, response):
+    token = None
+    for item in response.json()['items']:
+        user_data = json.loads(item['Content']['Body'])
+        user_login = user_data['Login']
+        if user_login == login:
+            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+            break
+    return token
+
+
+
