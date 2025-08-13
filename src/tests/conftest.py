c991f9b5bb2769dd237e97b38437a51d26212e76
Maksim Ivanov <mivanov@webmonitorx.ru>
2025-08-14 03:11:33 +1000

    добавили конфигуратор

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,24 +4,20 @@ from services.api_mailhog.api.mailhog_api import MailhogApi
 from services.dm_api_account.api.account_api import AccountApi
 from services.dm_api_account.api.login_api import LoginApi
 from config import BASE_API_URL, MAIL_URL
+from restclient.configuration import Configuration as MailhogConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
 
-
-@pytest.fixture()
-def base_api_url():
-    yield BASE_API_URL
-
-@pytest.fixture()
-def mail_url():
-    yield MAIL_URL
+dm_api_configuration = DmApiConfiguration(host=BASE_API_URL)
+mailhog_configuration = MailhogConfiguration(host=MAIL_URL)
 
 @pytest.fixture()
-def account_api(base_api_url):
-    yield AccountApi(host=base_api_url)
+def account_api():
+    yield AccountApi(configuration=dm_api_configuration)
 
 @pytest.fixture()
-def login_api(base_api_url):
-    yield LoginApi(host=base_api_url)
+def login_api():
+    yield LoginApi(configuration=dm_api_configuration)
 
 @pytest.fixture()
-def mailhog_api(mail_url):
-    yield MailhogApi(host=mail_url)
+def mailhog_api():
+    yield MailhogApi(configuration=mailhog_configuration)
