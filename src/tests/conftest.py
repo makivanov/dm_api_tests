0fcff62398619fbb52cebf0738a0f64bbfb5e1ed
Maksim Ivanov <mivanov@webmonitorx.ru>
2025-08-14 01:35:39 +1000

    rename to services

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 
-from endpoints.api_mailhog.api.mailhog_api import MailhogApi
-from endpoints.dm_api_account.api.account_api import AccountApi
-from endpoints.dm_api_account.api.login_api import LoginApi
+from services.api_mailhog.api.mailhog_api import MailhogApi
+from services.dm_api_account.api.account_api import AccountApi
+from services.dm_api_account.api.login_api import LoginApi
 from config import BASE_API_URL, MAIL_URL
 
 
